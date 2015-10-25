2f3bc6cd069a81cce4a249f4b3536a819bd5d024
BBBBlarry <blarrywang@gmail.com>
2015-10-25 19:52:42 -0400

    Worker Debugging

--- assets/js/blockWorker.js
+++ assets/js/blockWorker.js
@@ -2,6 +2,7 @@ var randomTime;
 
 while(true){
   randomTime = Math.floor(Math.random() * 2000); //less than 2s
+  postMessage(0); //debug message
   setTimeout(flip, randomTime);
   postMessage(1); //send signal to flip
   alert("Message Out");
