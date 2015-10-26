82c96f7924bacae3f256a22a1003141feb27fb32
BBBBlarry <blarrywang@gmail.com>
2015-10-25 20:34:32 -0400

    Worker Debugging
    Implementing of web worker thread code

--- assets/js/blockWorker.js
+++ assets/js/blockWorker.js
@@ -1,11 +1,12 @@
-var period;
+var period = 2000;
 
+setInterval(function() { postMessage(1) }, period);
 
-
+/*
 function sendPeriodicSignal() {
     period = Math.floor(Math.random() * 2000); //less than .2second
     postMessage(1);
     setTimeout("sendPeriodicSignal()", period);
 }
 
-sendPeriodicSignal();
+sendPeriodicSignal();*/
