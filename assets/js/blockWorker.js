0b8fc9e4f6bad13a652e4a37b0ebb1122037c947
BBBBlarry <blarrywang@gmail.com>
2015-10-25 20:17:48 -0400

    worker debugging

--- assets/js/blockWorker.js
+++ assets/js/blockWorker.js
@@ -5,7 +5,7 @@ var randomTime;
 function periodicSignal() {
     randomTime = Math.floor(Math.random() * 200); //less than .2second
     postMessage(1);
-    setTimeout("timedCount()",randomTime);
+    setTimeout("timedCount()",200);
 }
 
 periodicSignal();
