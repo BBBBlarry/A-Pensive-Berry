8f5dad8630947ce6eb8a862cd6bf6dcc090e79e4
BBBBlarry <blarrywang@gmail.com>
2015-10-25 20:04:17 -0400

    Worker debugging
    Test without infinite loop

--- assets/js/blockWorker.js
+++ assets/js/blockWorker.js
@@ -1,9 +1,9 @@
 var randomTime;
 
-while(true){
+
   randomTime = Math.floor(Math.random() * 2000); //less than 2s
   postMessage(0); //debug message
   setTimeout(flip, randomTime);
   postMessage(1); //send signal to flip
-}
+
 
